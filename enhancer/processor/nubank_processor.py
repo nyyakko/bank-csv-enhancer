8824519a83d6e6f926f603ac69bfd83c93c703bf
nyyakko <nyakonyns@gmail.com>
2026-03-14 16:53:25 -0300

    add refund processing

--- enhancer/processor/nubank_processor.py
+++ enhancer/processor/nubank_processor.py
@@ -18,7 +18,10 @@ class NubankProcessor(IProcessor):
         for row in reader:
             transaction = {}
 
-            if re.search(r"Transferência", row[-1], re.IGNORECASE):
+            if re.search(r"Estorno", row[-1], re.IGNORECASE):
+                transaction = process_refund(row)
+                transaction.descricao = f"Estorno {"de" if float(transaction.valor) > 0 else "para"} {transaction.nome_pessoa}"
+            elif re.search(r"Transferência", row[-1], re.IGNORECASE):
                 transaction = process_transfer(row)
                 transaction.descricao = f"Transferência {"de" if float(transaction.valor) > 0 else "para"} {transaction.nome_pessoa}"
             elif re.search(r"Pagamento", row[-1], re.IGNORECASE):
@@ -53,6 +56,30 @@ def process_transfer(row):
 
     nome_match = re.search(r"-\s*(.+?)\s*-", description)
     result.nome_pessoa = f"{nome_match.group(1).strip().title()} ({result.codigo_instituicao})" if nome_match else "__NOME_PESSOA__"
+
+    agencia_match = re.search(r"Agência:\s*(\d+)", description)
+    result.agencia = agencia_match.group(1) if agencia_match else "__AGENCIA__"
+
+    conta_match = re.search(r"Conta:\s*([\d\-]+)", description)
+    result.conta = conta_match.group(1) if conta_match else "__CONTA__"
+
+    return result
+
+def process_refund(row):
+    result = Transaction(identificador=row[2], data=row[0], valor=row[1], tipo_operacao="ESTORNO")
+    description = row[-1]
+
+    tipo_match = re.search(r"Transferência (\w+)", description, re.IGNORECASE)
+    result.tipo_movimentacao = tipo_match.group(1).upper() if tipo_match else "__TIPO_MOVIMENTACAO__"
+
+    inst_match = re.search(r"([^-]+\.\d{3}\.\d{3}-[^-]+|[^-]+\d{2}\.\d{3}\.\d{3}\/\d{4}-[^-]+)-([^-]*.*)\(\d+\)", description)
+    result.instituicao = inst_match.group(2).strip() if inst_match else "__INSTITUICAO__"
+
+    codigo_match = re.search(r"\((\d+)\)", description)
+    result.codigo_instituicao = codigo_match.group(1) if codigo_match else "__CODIGO_INSTITUICAO__"
+
+    nome_match = re.search(r"-\s*(.+?)\s*-\s*(.+?)\s*-([^-]+\.\d{3}\.\d{3}-[^-]+|[^-]+\d{2}\.\d{3}\.\d{3}\/\d{4}-[^-]+)", description)
+    result.nome_pessoa = f"{nome_match.group(2).strip().title()} ({result.codigo_instituicao})" if nome_match else "__NOME_PESSOA__"
 
     agencia_match = re.search(r"Agência:\s*(\d+)", description)
     result.agencia = agencia_match.group(1) if agencia_match else "__AGENCIA__"
